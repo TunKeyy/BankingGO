01575122b27d26110f001d38d55132e30871b5b9
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-07-22 15:35:11 +0700

    create schema and unit test

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	var err error
+	pool, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	testQueries = New(pool)
+
+	os.Exit(m.Run())
+}
