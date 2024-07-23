b44d936ccde23529564f2869a8de4a067bcf1d02
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-07-23 14:42:45 +0700

    edit name of relation

--- db/migration/000002_add_users.up.sql
+++ db/migration/000002_add_users.up.sql
@@ -7,6 +7,6 @@ CREATE TABLE "users" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "accounts" ADD FOREIGN KEY (owner) REFERENCES "user" ("username");
+ALTER TABLE "accounts" ADD FOREIGN KEY (owner) REFERENCES "users" ("username");
 
 ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
