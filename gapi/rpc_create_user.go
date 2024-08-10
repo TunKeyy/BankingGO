f23760006896110dc088b6252c8ab4e0711c8c16
KhaNguyennn <kha.nguyen@nexlab.tech>
2024-08-10 10:34:20 +0700

    fix bug google api and add validation

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -6,11 +6,18 @@ import (
 	db "github.com/TunKeyy/BankingGO/db/sqlc"
 	"github.com/TunKeyy/BankingGO/pb"
 	"github.com/TunKeyy/BankingGO/util"
+	"github.com/TunKeyy/BankingGO/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	violations := validateCreateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	hashedPassword, err := util.HashPassword(req.Password)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
@@ -36,4 +43,20 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	}
 
 	return rsp, nil
+}
+
+func validateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.Username); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := val.ValidatePassword(req.Password); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	if err := val.ValidateEmail(req.Email); err != nil {
+		violations = append(violations, fieldViolation("email", err))
+	}
+	if err := val.ValidateFullName(req.FullName); err != nil {
+		violations = append(violations, fieldViolation("full_name", err))
+	}
+	return violations
 }
